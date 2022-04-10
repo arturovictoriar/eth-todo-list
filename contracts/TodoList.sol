0fe0ca07ce7cb876d9407d929f0d1706c8cfb626
Arturo Victoria Rincon <arincon@colegium.com>
2022-04-10 11:54:43 -0500

    Connect blockchain with a website

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,7 +2,24 @@
 pragma solidity >=0.5.0;
 
 contract TodoList {
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
     uint public taskCount = 0;
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask('Check out arturovictoriar.com');
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 
 
 }
